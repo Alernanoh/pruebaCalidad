627f2e3277c0d1bf39b25ee501a60449dd0092a9
Nano <alhernano@gmail.com>
2025-03-07 09:24:57 -0500

    Addded load function to the service

--- drivers_prueba/services/drivers_service.py
+++ drivers_prueba/services/drivers_service.py
@@ -2,6 +2,8 @@ from django.http import response
 import requests 
 from django.shortcuts import render, get_list_or_404
 
+from models import Driver
+
 
 API_URL = "https://f1api.dev/api/drivers"
 
@@ -17,4 +19,16 @@ def get_driver(driverId):
         return response.json()
     return []
 
-
+def load_drivers():
+    if not Driver.objects.count():
+        drivers = get_drivers()
+        for driver in drivers:
+            Driver.objects.create(
+                name=driver["name"],
+                surname=driver["surname"],
+                nationality=driver["nationality"],
+                birthday=driver["birthday"],
+                url=driver["url"],
+            )
+        return f"{len(drivers)} conductores cargados"
+    return "Los productos ya estan en la base de datos"
