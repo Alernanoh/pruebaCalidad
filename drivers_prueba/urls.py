50a5c6b6ec1eba5a27f5bd2e24bbf5fbb2fedb62
Nano <alhernano@gmail.com>
2025-03-07 09:16:59 -0500

    Removed unnecesary imports

--- drivers_prueba/urls.py
+++ drivers_prueba/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import driver_list, driver_detail
 from . import views
 
 urlpatterns = [
-    path("", views.driver_list, name="driver_list"),
+    path(" ", views.driver_list, name="driver_list"),
     path("driver/<int:id>", views.driver_detail, name="driver_detail"),
 ]
--- drivers_prueba/views.py
+++ drivers_prueba/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 
-from drivers_prueba.services.drivers_service import get_drivers, get_driver
+from services import drivers_service
+
 
 
 # Create your views here.
 def driver_list(request):
-    drivers = get_drivers()
+    drivers = drivers_service.get_drivers()
     return render(request, 'driver_list.html', {'drivers': drivers})
 
-def driver_detail(request, id):
-    driver = get_driver(id)
+def driver_detail(request, driverId):
+    driver = drivers_service.get_driver(driverId)
     return render(request, 'driver_detail.html', {"driver": driver})
